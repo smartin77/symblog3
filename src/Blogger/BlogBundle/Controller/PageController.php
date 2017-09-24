385d199908627d265b242a427117a6a5fd9c0320
smartin77 <svec@vadium.sk>
2017-09-25 00:21:37 +0200

    [PART2] Contact page - Form

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -5,6 +5,9 @@
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
@@ -18,9 +21,26 @@ class PageController extends Controller
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
     }
 
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('BloggerBlogBundle:Page:contact.html.twig');
+        $enquiry = new Enquiry();
+        $form = $this->createForm(EnquiryType::class, $enquiry);
+
+        $form->handleRequest($request);
+
+        if ($request->getMethod() == 'POST') {
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 
 }
