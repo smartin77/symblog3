acc1ac3e349a5e6f63f4017517542330eabad9df
smartin77 <svec@vadium.sk>
2017-09-27 00:11:53 +0200

    [PART2] Contact page -  Swift Mailer

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -28,9 +28,17 @@ class PageController extends Controller
 
         $form->handleRequest($request);
 
-        if ($request->getMethod() == 'POST') {
+        if ($request->isMethod('POST')) {
             if ($form->isValid()) {
-                // Perform some action, such as sending an email
+
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                    ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                $this->addFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
 
                 // Redirect - This is important to prevent users re-posting
                 // the form if they refresh the page
