f3929cbd012f187da4b2a4c12e70fd4dc90c7cac
smartin77 <svec@vadium.sk>
2018-01-15 11:59:07 +0100

    [PART6] Functional testing - Testing the About page

---
+++ tests/Blogger/BlogBundle/Controller/PageControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+// tests/Blogger/BlogBundle/Controller/PageControllerTest.php
+
+namespace Blogger\BlogBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PageControllerTest extends WebTestCase
+{
+    public function testAbout()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/about');
+
+        $this->assertEquals(1, $crawler->filter('h1:contains("About symblog")')->count());
+    }
+}
